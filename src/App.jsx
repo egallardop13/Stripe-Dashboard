45dfbbe42e61c7ef75d0687ae62626f78b8cde5a
egallardop13 <egallardodev@gmail.com>
2024-08-07 20:25:21 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarComponent from "./components/SidebarComponent";
+
+const App = () => {
+  return (
+    <div>
+      <SidebarComponent />
+    </div>
+  );
+};
+
+export default App;
